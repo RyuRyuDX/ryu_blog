c2083bef58cb35a07711ccbc877654348812d298
RyuRyuDX <gibson.red.gin.suke@gmail.com>
2023-07-05 03:18:00 +0900

    【ADD】Cors対応やGemを追加

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -63,6 +63,11 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "api"
+  # config.hosts << "api"
+
+   # Hostname
+   config.hosts << 'localhost'
+   config.hosts << '127.0.0.1'
+   config.hosts << ENV['BACKEND_HOST']
 
 end
